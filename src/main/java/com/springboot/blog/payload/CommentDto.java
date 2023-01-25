eb61ec109f94a2d177e956b6d2c09c317b978033
Mainuddin <web97development@gmail.com>
2023-01-25 09:07:53 +0530

    Exception and Validation Completed For comment and post

--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,5 +1,8 @@
 package com.springboot.blog.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,18 @@ import lombok.NoArgsConstructor;
 @Data
 public class CommentDto {
     private long id;
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+
+    // email should not be null or empty
+    // email field validation
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
     private String email;
+
+    // comment body should not be bull or empty
+    // Comment body must be minimum 10 characters
+    @NotEmpty
+    @Size(min = 10, message = "Comment body must be minimum 10 characters")
     private String body;
 }
