8c8297cd0a65f7c41dc419cfb5cc09f122147fb2
Mainuddin <web97development@gmail.com>
2023-01-28 03:40:10 +0530

    develop working login and register api

--- src/main/java/com/springboot/blog/config/SecurityConfig.java
+++ src/main/java/com/springboot/blog/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.springboot.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -30,9 +31,10 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf().disable()
                 .authorizeHttpRequests((authorize)->
-                        authorize.requestMatchers("/api/**").permitAll()
+                        authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
+                                .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
-                ).httpBasic(Customizer.withDefaults());
+                );
         return http.build();
     }
 
---
+++ src/main/java/com/springboot/blog/payload/RegisterDto.java
@@ -0,0 +1,15 @@
+package com.springboot.blog.payload;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterDto {
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+}
